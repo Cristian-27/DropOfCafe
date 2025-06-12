94447b9951cf7f2056ce4de7ef6a50f31b178955
Cristian <cristian.sc2710@gmail.com>
2025-06-12 14:05:41 -0400

    Primer avance de proyecto

---
+++ src/i18n/ui.ts
@@ -0,0 +1,28 @@
+export const languages = {
+  en: 'English',
+  es: 'Español',
+  ru: 'Русский',
+};
+
+export const defaultLang = 'es';
+
+export const ui ={
+
+    en:{
+        'navItem1': 'Home',
+        'navItem2': 'About',
+        'navItem3': 'Contact',
+    },
+    es:{
+        'navItem1': 'Inicio',
+        'navItem2': 'Acerca de',
+        'navItem3': 'Contacto',
+    },
+    ru:{
+        'navItem1': 'Главная',
+        'navItem2': 'О нас',
+        'navItem3': 'Контакты',
+    }
+} as const;
+
+export const showDefaultLang = false;
